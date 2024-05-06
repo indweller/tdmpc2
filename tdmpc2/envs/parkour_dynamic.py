3e33a498068830cc923cc50bfd34a22df11ad581
Prashanth Ravichandar <rprash99@gmail.com>
2024-05-06 16:21:13 -0700

    Add hp search

--- tdmpc2/envs/parkour_dynamic.py
+++ tdmpc2/envs/parkour_dynamic.py
@@ -8,6 +8,9 @@ class ParkourDynamic(ParkourEnv):
         self.gap = 0.3
         self.obstacle_oh = {'rotating_disc': np.array([0, 0]), 'moving_cart': np.array([0, 1]), 'stewart_platform': np.array([1, 0]), 'stairs': np.array([1, 1])}
         self.obstacle_locs = {'rotating_disc': [0, 0], 'moving_cart': [0, 0], 'stewart_platform': [0, 0], 'stairs': [0, 0]}
+        # self.obstacle_oh = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [-1, -1]])
+        # self.obstacle_indices = np.array([[24, 25, 26], [32, 33, 34], [43, 44, 45], [54, 55, 56]])
+        # self.obstacle_locs = np.zeros((4, 2))
         super().__init__(cfg, exp_conf_path)
         self.max_episode_steps = 10
     
@@ -28,15 +31,18 @@ class ParkourDynamic(ParkourEnv):
                 self.sim.simulate_n_steps(n_steps=1)
         current_pose = self.sim.data.qpos[:3].copy()
         reward = self.get_reward(prev_pose, current_pose)
-        done = False
+        done = self.check_done(current_pose)
+        return self.get_high_level_obs(action), reward, done, {}
+    
+    def check_done(self, current_pose):
         terrain_height = self.sim.get_terrain_height_at(current_pose)
         if self.sim.data.qpos[2] < 0.35 or self.sim.data.qpos[2] - terrain_height < 0.35:
-            done = True
+            return True
         if abs(self.sim.data.qpos[1]) > 0.75:
-            done = True
+            return True
         if self.current_step >= self.max_episode_steps:
-            done = True
-        return self.get_high_level_obs(action), reward, done, {}
+            return True
+        return False
 
     def get_reward(self, prev_pose, current_pose):
         return self.forward_reward_weight * np.exp(-0.15 * np.abs(current_pose[0] - 10))
@@ -58,6 +64,50 @@ class ParkourDynamic(ParkourEnv):
                 return high_level_obs
         high_level_obs = np.concatenate((high_level_obs, np.array([-1, -1]), np.array([-1, -1])))
         return high_level_obs
+
+    # def get_high_level_obs(self, mode_latent):
+    #     high_level_obs = super().get_high_level_obs(mode_latent)
+    #     base_pos = self.get_robot_base_pos()
+    #     # Check if base_pos is within the location range of any obstacle
+    #     in_range = np.logical_and(base_pos[0] >= self.obstacle_locs[:, 0], base_pos[0] <= self.obstacle_locs[:, 1])
+    #     if np.any(in_range):
+    #         # Find the indices of the current and next obstacles
+    #         current_obstacle_index = np.argmax(in_range)
+    #         # Concatenate the one-hot encodings of the current and next obstacles to the high-level observation
+    #         high_level_obs = np.concatenate((high_level_obs, self.obstacle_oh[current_obstacle_index], self.obstacle_oh[current_obstacle_index + 1]))
+    #     else:
+    #         # Concatenate two [-1, -1] arrays to the high-level observation
+    #         high_level_obs = np.concatenate((high_level_obs, np.array([-1, -1]), np.array([-1, -1])))
+    #     return high_level_obs
+
+    # def set_obstacles(self):
+    #     # Get the x-coordinates of the obstacles
+    #     x_coords = self.sim.data.qpos[self.obstacle_indices[:, 0]].copy()
+    #     x_coords += -0.5 * np.arange(1, len(self.obstacle_indices) + 1)
+    #     x_coords += self.gap * np.arange(1, len(self.obstacle_indices) + 1)
+    #     # Shuffle the x-coordinates
+    #     np.random.shuffle(x_coords)
+    #     # Set the new x-coordinates of the obstacles
+    #     self.sim.data.qpos[self.obstacle_indices[:, 0]] = x_coords
+    #     # Update the obstacle locations
+    #     self.obstacle_locs[:, 0] = x_coords - 0.75
+    #     self.obstacle_locs[:, 1] = x_coords + 0.75  
+
+    # def set_obstacles_new(self):
+    #     # Get the x-coordinates of the obstacles
+    #     x_coords = self.sim.data.qpos[self.obstacle_indices[:, 0]].copy()
+    #     # Store the original first three locations
+    #     original_first_three = x_coords[:3].copy()
+    #     # Set the first obstacle to -200
+    #     x_coords[0] = -200
+    #     # Shuffle the original first three locations and assign them to the last three obstacles
+    #     np.random.shuffle(original_first_three)
+    #     x_coords[1:4] = original_first_three
+    #     # Set the new positions
+    #     self.sim.data.qpos[self.obstacle_indices[:, 0]] = x_coords
+    #     # Update the obstacle locations
+    #     self.obstacle_locs[:, 0] = x_coords - 0.75
+    #     self.obstacle_locs[:, 1] = x_coords + 0.75
 
     def set_obstacles(self):
         rotating_disc =  np.take(self.sim.data.qpos, [24, 25, 26])
@@ -81,6 +131,37 @@ class ParkourDynamic(ParkourEnv):
         np.put(self.sim.data.qpos, [43, 44, 45], stewart_platform)
         np.put(self.sim.data.qpos, [54, 55, 56], stairs)
         self.obstacle_locs = {'rotating_disc': [rotating_disc[0] - 0.75, rotating_disc[0] + 0.75], 'moving_cart': [moving_cart[0] - 0.75, moving_cart[0] + 0.75], 'stewart_platform': [stewart_platform[0] - 0.75, stewart_platform[0] + 0.75], 'stairs': [stairs[0] - 0.75, stairs[0] + 0.75]}
-        
-
-
+    
+    def set_obstacles_new(self):
+        rotating_disc =  np.take(self.sim.data.qpos, [24, 25, 26])
+        moving_cart = np.take(self.sim.data.qpos, [32, 33, 34])
+        stewart_platform = np.take(self.sim.data.qpos, [43, 44, 45])
+        stairs = np.take(self.sim.data.qpos, [54, 55, 56])
+        # rotating_disc, moving_cart, stewart_platform, stairs = obs.obstacle_state(self)
+        stairs[0] = stewart_platform[0]
+        stewart_platform[0] = moving_cart[0]
+        moving_cart[0] = rotating_disc[0]
+        rotating_disc[0] = 7.25
+        #print("Old positions of obstacles: ", rotating_disc[0], moving_cart[0], stewart_platform[0], stairs[0])
+        x_coords = np.array([moving_cart[0], stewart_platform[0], stairs[0]])
+        # x_coords -= 1.5
+        #print(x_coords)
+        x_coords += -0.5 * (np.arange(3) + 1)
+        #print(x_coords)
+        x_coords += self.gap * (np.arange(3) + 1)
+        #print(x_coords)
+        # Shuffle the x_coords
+        np.random.shuffle(x_coords)
+        # Set the new positions
+        rotating_disc[0] = -200
+        moving_cart[0] = x_coords[0]
+        stewart_platform[0] = x_coords[1]
+        stairs[0] = x_coords[2]
+        # Set the new positions
+        # Print new positions of all obstacles with 3 decimal places
+        #print("New positions of obstacles: ", rotating_disc[0], moving_cart[0], stewart_platform[0], stairs[0])
+        np.put(self.sim.data.qpos, [24, 25, 26], rotating_disc)
+        np.put(self.sim.data.qpos, [32, 33, 34], moving_cart)
+        np.put(self.sim.data.qpos, [43, 44, 45], stewart_platform)
+        np.put(self.sim.data.qpos, [54, 55, 56], stairs)
+        self.obstacle_locs = {'rotating_disc': [rotating_disc[0] - 0.75, rotating_disc[0] + 0.75], 'moving_cart': [moving_cart[0] - 0.75, moving_cart[0] + 0.75], 'stewart_platform': [stewart_platform[0] - 0.75, stewart_platform[0] + 0.75], 'stairs': [stairs[0] - 0.75, stairs[0] + 0.75]}
--- tdmpc2/ppo.py
+++ tdmpc2/ppo.py
@@ -1,20 +1,19 @@
 # Write a PPO agent for the ParkourEnv environment using stable baselines3.
 
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import sys
-import os
 import numpy as np
 from envs.parkour import ParkourEnv
+from envs.parkour_dynamic import ParkourDynamic
 from common.parser import parse_cfg
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.callbacks import EvalCallback
 import hydra
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
 
 class PPOParkourEnv(ParkourEnv):
     def __init__(self, cfg, exp_conf_path):
@@ -30,6 +29,20 @@ class PPOParkourEnv(ParkourEnv):
     
     def get_reward(self, prev_pose, current_pose):
         return self.forward_reward_weight * np.exp(-0.15 * np.abs(current_pose[0] - 10))
+    
+class PPOParkourDynamicEnv(ParkourDynamic):
+    def __init__(self, cfg, exp_conf_path):
+        super(PPOParkourDynamicEnv, self).__init__(cfg, exp_conf_path)
+    
+    def step(self, action):
+        obs, reward, done, info = super(PPOParkourDynamicEnv, self).step(action)
+        return obs, reward, done, False, info
+    
+    def reset(self, seed=0):
+        return super(PPOParkourDynamicEnv, self).reset(), {}
+    
+    def get_reward(self, prev_pose, current_pose):
+        return self.forward_reward_weight * np.exp(-0.15 * np.abs(current_pose[0] - 10))
 
 @hydra.main(version_base=None, config_name='config', config_path='/home/learning/prashanth/tdmpc2/tdmpc2/')
 def train_ppo(cfg: dict):
@@ -40,11 +53,11 @@ def train_ppo(cfg: dict):
     # check_env(env)
     from functools import partial
     env_fn = partial(make_env, cfg)
-    vec_env = make_vec_env(env_fn, n_envs=30, vec_env_cls=SubprocVecEnv)
+    vec_env = make_vec_env(env_fn, n_envs=2, vec_env_cls=SubprocVecEnv)
     eval_env = make_vec_env(env_fn, n_envs=1, vec_env_cls=SubprocVecEnv)
     ppo_args = {}
     ppo_args["batch_size"] = 64
-    ppo_args["n_steps"] = 2048
+    ppo_args["n_steps"] = 128
     ppo_args["gamma"]= 0.95
     ppo_args["learning_rate"] = 3.56987e-05
     ppo_args["ent_coef"] = 0.00238306
@@ -61,47 +74,106 @@ def train_ppo(cfg: dict):
                     )
     model = PPO('MlpPolicy', vec_env, tensorboard_log='/home/learning/prashanth/tdmpc2/tdmpc2/sb3/logs/', verbose=0, **ppo_args)
 
-    eval_callback = EvalCallback(eval_env, best_model_save_path='/home/learning/prashanth/tdmpc2/tdmpc2/sb3/', log_path='/home/learning/prashanth/tdmpc2/tdmpc2/sb3/', eval_freq=2049, deterministic=True)
+    eval_callback = EvalCallback(eval_env, best_model_save_path='/home/learning/prashanth/tdmpc2/tdmpc2/sb3/tmp', log_path='/home/learning/prashanth/tdmpc2/tdmpc2/sb3/tmp', eval_freq=2049, deterministic=True)
     
     model.learn(total_timesteps=cfg.steps, progress_bar=True, callback=eval_callback)
     
-    model.save('/home/learning/prashanth/tdmpc2/tdmpc2/sb3/final')
+    model.save('/home/learning/prashanth/tdmpc2/tdmpc2/sb3/finaltmp')
 
-@hydra.main(version_base=None, config_name='config', config_path='/home/learning/prashanth/tdmpc2/tdmpc2/')
+@hydra.main(version_base=None, config_name='config_dynamic', config_path='/home/learning/prashanth/tdmpc2/tdmpc2/')
 def eval_ppo(cfg: dict):
     cfg = parse_cfg(cfg)
     def make_env(cfg):
-        env = PPOParkourEnv(cfg, cfg.exp_conf_path)
+        env = PPOParkourDynamicEnv(cfg, cfg.exp_conf_path)
         return env
     from functools import partial
     env_fn = partial(make_env, cfg)
-    # vec_env = make_vec_env(env_fn, n_envs=1, vec_env_cls=SubprocVecEnv)
     vec_env = env_fn()
-    model = PPO.load('/home/learning/prashanth/tdmpc2/tdmpc2/best_model.zip')
-    for _ in range(10):
+    model = PPO.load('/home/learning/prashanth/tdmpc2/tdmpc2/sb3/best_model.zip')
+    vec_env.sim.viewer.cam.lookat = [4.05, -1.5, 0]
+    vec_env.sim.viewer.cam.distance = 2.5
+    vec_env.sim.viewer.cam.elevation = -20
+    vec_env.sim.viewer.cam.azimuth = 135
+    all_returns = []
+    all_episode_lengths = []
+    all_final_x = []
+    for _ in range(20):
         obs, _ = vec_env.reset()
         done = False
         vec_env.sim.viewer_paused = True
         vec_env.sim.viewer.update_hfield(0)
+        returns = 0
+        steps = 0
         while not done:
             if not vec_env.sim.viewer_paused:
                 vec_env.sim.viewer.sync()
                 # print(model.predict(obs))
                 action, _states = model.predict(obs)
+                # action = np.random.normal(0, 0.05, 2).clip(-1, 1)
+            #action = np.random.uniform(-1, 1, 2)
                 obs, rewards, done, _, info = vec_env.step(action)
-                print()
-                # vec_env.render("human")
+                returns += rewards
+                steps += 1
+        
+        print(f'Episode finished after {steps} steps with return {returns}. Final x = {vec_env.sim.data.qpos[0]}')
+        all_returns.append(returns)
+        all_episode_lengths.append(steps)
+        all_final_x.append(vec_env.sim.data.qpos[0])
     vec_env.close()
+    print(f'Mean return: {np.mean(all_returns)} +/- {np.std(all_returns)}')
+    print(f'Mean episode length: {np.mean(all_episode_lengths)} +/- {np.std(all_episode_lengths)}')
+    print(f'Mean final x: {np.mean(all_final_x)} +/- {np.std(all_final_x)}')
+
+@hydra.main(version_base=None, config_name='config_dynamic', config_path='/home/learning/prashanth/tdmpc2/tdmpc2/')
+def hyper_param_search(cfg: dict):
+    cfg = parse_cfg(cfg)
+    cfg.steps = 122_880
+    # cfg.steps = 600
+    def make_env(cfg):
+        env = PPOParkourDynamicEnv(cfg, cfg.exp_conf_path)
+        return env
+    from functools import partial
+    env_fn = partial(make_env, cfg)
+    ppo_args = {}
+    ppo_args["gamma"]= 0.95
+    ppo_args["learning_rate"] = 3.56987e-05
+    ppo_args["ent_coef"] = 0.00238306
+    ppo_args["clip_range"] = 0.3
+    ppo_args["n_epochs"] = 5
+    ppo_args["gae_lambda"] = 0.9
+    ppo_args["max_grad_norm"] = 2
+    ppo_args["vf_coef"] = 0.431892
+    ppo_args["policy_kwargs"] = dict(
+                    log_std_init =-2,
+                    ortho_init =False,
+                    activation_fn =nn.Tanh,
+                    net_arch =dict(pi =[128, 128], vf =[128, 128])
+                    )
+    n_envs = [16, 24, 30]
+    batch_sizes = [32, 64, 128, 256, 512]
+    n_steps = [256, 512, 1024, 2048]
+    # n_envs = [15, 25]
+    # batch_sizes = [64, 128]
+    # n_steps = [20, 40]
+    for n_env in n_envs:
+        for batch_size in batch_sizes:
+            for n_step in n_steps:
+                print("-------------------")
+                print(f"n_env: {n_env}, batch_size: {batch_size}, n_step: {n_step}")
+                ppo_args["batch_size"] = batch_size
+                ppo_args["n_steps"] = n_step
+                vec_env = make_vec_env(env_fn, n_envs=n_env, vec_env_cls=SubprocVecEnv)
+                eval_env = make_vec_env(env_fn, n_envs=1, vec_env_cls=SubprocVecEnv)
+                model = PPO('MlpPolicy', vec_env, tensorboard_log=f'/home/learning/prashanth/tdmpc2/tdmpc2/sb3/hp/{n_env}_{batch_size}_{n_step}', verbose=0, **ppo_args)
+
+                eval_callback = EvalCallback(eval_env, best_model_save_path='/home/learning/prashanth/tdmpc2/tdmpc2/sb3/hp', log_path='/home/learning/prashanth/tdmpc2/tdmpc2/sb3/hp', eval_freq=n_step+1, deterministic=True)
+                
+                model.learn(total_timesteps=cfg.steps, progress_bar=True, callback=eval_callback)
+                
+                model.save('/home/learning/prashanth/tdmpc2/tdmpc2/sb3/hp/final')
+                print("-------------------")
 
 if __name__ == '__main__':
-    # cfg = {
-    #     'env_name': 'ParkourEnv',
-    #     'n_steps': 2048,
-    #     'batch_size': 64,
-    #     'lr': 3e-4,
-    #     'total_timesteps': 100000,
-    #     'save_path': 'ppo_parkour'
-    # }
-    train_ppo()
+    # train_ppo()
     # eval_ppo()
-
+    hyper_param_search()
