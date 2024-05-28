245277fe1da1e83ce8b1bf09d94334b9020922ab
Prashanth Ravichandar <rprash99@gmail.com>
2024-05-27 22:08:13 -0700

    Add SAC

--- tdmpc2/ppo.py
+++ tdmpc2/ppo.py
@@ -4,7 +4,7 @@ import numpy as np
 from envs.parkour import ParkourEnv
 from envs.parkour_dynamic import ParkourDynamic
 from common.parser import parse_cfg
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.env_checker import check_env
@@ -132,7 +132,6 @@ def eval_ppo(cfg: dict):
 def hyper_param_search(cfg: dict):
     cfg = OmegaConf.to_container(cfg)
     cfg["steps"] = 122_880
-    # cfg.steps = 600
     def make_env(cfg):
         env = PPOParkourDynamicEnv(cfg)
         return env
@@ -177,7 +176,84 @@ def hyper_param_search(cfg: dict):
                 model.save('sb3/hp/final')
                 print("-------------------")
 
+@hydra.main(version_base=None, config_name='dynamic', config_path=CONFIG_PATH)
+def train_sac(cfg: dict):
+    cfg = OmegaConf.to_container(cfg)
+    def make_env(cfg):
+        env = PPOParkourDynamicEnv(cfg)
+        return env
+    # check_env(env)
+    from functools import partial
+    env_fn = partial(make_env, cfg)
+    vec_env = make_vec_env(env_fn, n_envs=12, vec_env_cls=SubprocVecEnv)
+    eval_env = make_vec_env(env_fn, n_envs=1, vec_env_cls=SubprocVecEnv)
+    sac_args = {}
+    sac_args["learning_starts"] = 10_000
+    sac_args["policy_kwargs"] = dict(activation_fn =nn.Tanh,
+                    net_arch =[128, 128]
+                    )
+    eval_freq = 201
+    model = SAC('MlpPolicy', vec_env, tensorboard_log='sb3/logs/sac_low_oracle_freq', verbose=0, **sac_args)
+
+    eval_callback = EvalCallback(eval_env, best_model_save_path='sb3/sac_low_oracle_freq', log_path='sb3/sac_low_oracle_freq', eval_freq=eval_freq, deterministic=True)
+    
+    model.learn(total_timesteps=cfg["steps"], progress_bar=True, callback=eval_callback)
+    
+    model.save('sb3/sac_low_oracle_freq')
+
+@hydra.main(version_base=None, config_name='dynamic', config_path=CONFIG_PATH)
+def eval_sac(cfg: dict):
+    cfg = OmegaConf.to_container(cfg)
+    def make_env(cfg):
+        env = PPOParkourDynamicEnv(cfg)
+        return env
+    from functools import partial
+    env_fn = partial(make_env, cfg)
+    vec_env = env_fn()
+    model = SAC.load('sb3/sac_low_oracle_freq/best_model.zip')
+    render = vec_env.render_viewer
+    vec_env.sim.viewer_paused = False
+    if render:
+        vec_env.sim.viewer.cam.lookat = [4.05, 0, 0]
+        vec_env.sim.viewer.cam.distance = 6.5
+        vec_env.sim.viewer.cam.elevation = -10
+        vec_env.sim.viewer.cam.azimuth = 90
+    all_returns = []
+    all_episode_lengths = []
+    all_final_x = []
+    all_times = []
+    for _ in range(20):
+        obs, _ = vec_env.reset()
+        done = False
+        if render:
+            vec_env.sim.viewer_paused = True
+            vec_env.sim.viewer.update_hfield(0)
+        returns = 0
+        steps = 0
+        import time
+        st = time.time()
+        while not done:
+            if render and not vec_env.sim.viewer_paused:
+                # print(model.predict(obs))
+                action, _states = model.predict(obs)
+                # action = np.random.normal(0, 0.05, 2).clip(-1, 1)
+                # action = np.random.uniform(-1, 1, 2)
+                obs, rewards, done, _, info = vec_env.step(action)
+                returns += rewards
+                steps += 1
+        end = time.time()
+        print(f'Episode finished after {steps} steps with return {returns:.2f}. Final x = {vec_env.sim.data.qpos[0]:.2f}')
+        all_returns.append(returns)
+        all_episode_lengths.append(steps)
+        all_final_x.append(vec_env.sim.data.qpos[0])
+        all_times.append((end - st) / steps)
+    vec_env.close()
+    print(f'Mean return: {np.mean(all_returns):.2f} +/- {np.std(all_returns):.2f}')
+    print(f'Mean episode length: {np.mean(all_episode_lengths):.2f} +/- {np.std(all_episode_lengths):.2f}')
+
 if __name__ == '__main__':
     # train_ppo()
-    eval_ppo()
+    # eval_ppo()
     # hyper_param_search()
+    # train_sac()
+    eval_sac()
